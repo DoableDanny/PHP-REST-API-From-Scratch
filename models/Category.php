97d36f6cb10ed0948fbdbaa828fc24cdcfc6fed5
Dan <dantheado@gmail.com>
2020-12-30 20:27:18 +0000

    Added read_single method

--- models/Category.php
+++ models/Category.php
@@ -14,7 +14,7 @@
       $this->conn = $db;
     }
 
-    // Get categories
+    // Get all categories
     public function read() {
       // Create query 
       $query = 'SELECT
@@ -32,6 +32,32 @@
       $stmt->execute();
 
       return $stmt;
+    }
+
+    // Get single category
+    public function read_single() {
+      // Query
+      $query = 'SELECT 
+        id,
+        name 
+      FROM 
+        ' . $this->table . '
+      WHERE 
+        id = ?';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+      
+      // Bind param
+      $stmt->bindParam(1, $this->id);
+
+      // Execute statement
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      $this->id = $row['id'];
+      $this->name = $row['name'];
     }
 
   }
