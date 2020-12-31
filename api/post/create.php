dd697971c4e4ad51f070d585684a7512028df98f
Dan <dantheado@gmail.com>
2020-12-31 16:36:20 +0000

    Formatting

--- api/post/create.php
+++ api/post/create.php
@@ -16,12 +16,12 @@ $db = $database->connect();
 $post = new Post($db);
 
 // Get raw posted data
-  $data = json_decode(file_get_contents("php://input"));
+$data = json_decode(file_get_contents("php://input"));
 
-  $post->title = $data->title;
-  $post->body = $data->body;
-  $post->author = $data->author;
-  $post->category_id = $data->category_id;
+$post->title = $data->title;
+$post->body = $data->body;
+$post->author = $data->author;
+$post->category_id = $data->category_id;
 
 if($post->create()) {
   echo json_encode(
